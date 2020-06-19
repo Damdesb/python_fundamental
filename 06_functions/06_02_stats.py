7413065265f9271c10af5a276ea9c5844dd81040
Dam Desb <nataliemarshall@Natalies-MacBook-Air.local>
2020-06-19 11:02:01 +1000

    functions

--- 06_functions/06_02_stats.py
+++ 06_functions/06_02_stats.py
@@ -6,8 +6,21 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
+
+# define the function here
+def stats(list_of_number):
+    for numb_ in list_of_number:
+        max_numb = max(list_of_number)
+        print('the max number is: ', max_numb)
+        min_numb = min(numb_)
+        print('the min number is : ', min_numb)
+        sum_1 = 0
+        sum_1 += numb_
+        print("the sum is : ", sum_1)
+        average = sum_1 / len(numb_)
+        print('the average is : ', average)
 
 # call the function below here
+
+
+stats(example_list)
--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -3,3 +3,18 @@ Write a program with 3 functions. Each function must call
 at least one other function and use the return value to do something.
 
 '''
+
+
+def lyrics():
+    print('frere jaques')
+    print('dormez vous?')
+
+def repeat_lyrics():
+    print(lyrics())
+    print(lyrics())
+
+def full_song():
+    print(repeat_lyrics())
+    print('sonnez les matines. Ding, dung dong')
+
+full_song()
